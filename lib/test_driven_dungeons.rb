925db1db2fedf81e7ee4f982538fc5cd9a22cf70
Ignatius Coulter <ignatiuscoulter@gmail.com>
2020-10-24 13:47:09 -0400

    Creating a Ruby project

---
+++ lib/test_driven_dungeons.rb
@@ -0,0 +1,6 @@
+require "test_driven_dungeons/version"
+
+module TestDrivenDungeons
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/test_driven_dungeons/version.rb
@@ -0,0 +1,3 @@
+module TestDrivenDungeons
+  VERSION = "0.1.0"
+end
