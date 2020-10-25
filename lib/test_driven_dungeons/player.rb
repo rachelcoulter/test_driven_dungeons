7bd59579c3283c2126e3647f6f90a6c0a33dc840
Ignatius Coulter <ignatiuscoulter@gmail.com>
2020-10-25 15:39:47 -0400

    A player can have multiple characters.

--- lib/test_driven_dungeons/player.rb
+++ lib/test_driven_dungeons/player.rb
@@ -1,6 +1,15 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :characters
+
   def initialize(name: nil)
     @name = name
+  end
+
+  def add_character(character)
+    characters << character
+  end
+
+  def characters
+    @characters ||= []
   end
 end
