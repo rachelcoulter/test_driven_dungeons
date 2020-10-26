5aa28e10e2c598ab4613508b05b333f58019403c
Ignatius Coulter <ignatiuscoulter@gmail.com>
2020-10-26 13:22:10 -0400

    A character has validated constitution.

--- lib/test_driven_dungeons/character.rb
+++ lib/test_driven_dungeons/character.rb
@@ -1,5 +1,5 @@
 class Character
-  attr_reader :name, :xp, :strength, :race, :dexterity
+  attr_reader :name, :xp, :strength, :race, :dexterity, :constitution
 
   def initialize(name: nil, race: nil)
     @name = name
@@ -23,7 +23,14 @@ class Character
     @dexterity = dexterity
   end
 
+  def constitution=(constitution)
+    validate_d20 constitution
+
+
+    @constitution = constitution
+  end
   def validate_d20(value)
     raise ArgumentError.new if value < 1 || value > 20
+
   end
 end
